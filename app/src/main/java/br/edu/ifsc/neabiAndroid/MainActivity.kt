57bd45be58d6173694fc6241dac7b31526b8a9c5
Bruno Perdona <bruno15perdona@gmail.com>
2022-07-02 16:20:18 -0300

    Adding base file to API

--- app/src/main/java/br/edu/ifsc/neabiAndroid/MainActivity.kt
+++ app/src/main/java/br/edu/ifsc/neabiAndroid/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    NeabicanApp()
                 }
             }
         }
@@ -30,14 +30,6 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    NeabiAndroidTheme {
-        Greeting("Android")
-    }
+fun NeabicanApp() {
+    Text(text = "Test")
 }
--- app/src/main/java/br/edu/ifsc/neabiAndroid/source/NeabicanApi.kt
+++ app/src/main/java/br/edu/ifsc/neabiAndroid/source/NeabicanApi.kt
@@ -1,3 +1,33 @@
 package br.edu.ifsc.neabiAndroid.source
 
+import br.edu.ifsc.neabiAndroid.source.model.SourceCampus
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
 
+//Link para recuperar imagens.
+//Ex: https://neabican.pythonanywhere.com/media/fotos_campus/campus-canoinhas.jpg
+
+private const val BASE_URL = "https://neabican.pythonanywhere.com"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface NeabicanApiService{
+    @GET("api/campus")
+    suspend fun getCampus(): List<SourceCampus>
+}
+
+object NeabicanApi{
+    val retrofitService: NeabicanApiService by lazy {
+        retrofit.create(NeabicanApiService::class.java)
+    }
+}
