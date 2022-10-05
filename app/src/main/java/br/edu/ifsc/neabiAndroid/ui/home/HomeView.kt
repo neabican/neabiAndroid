e012750a92539075f9b801e4f9c122221f99b66a
Gustavo Ziger <gustavoziger@gmail.com>
2022-10-04 21:08:15 -0300

    Added home search field logic

--- app/src/main/java/br/edu/ifsc/neabiAndroid/ui/home/HomeView.kt
+++ app/src/main/java/br/edu/ifsc/neabiAndroid/ui/home/HomeView.kt
@@ -1,54 +1,53 @@
 package br.edu.ifsc.neabiAndroid.ui.home
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import br.edu.ifsc.neabiAndroid.domain.model.Campus
 
 import br.edu.ifsc.neabiAndroid.ui.home.components.CampusCard
-import br.edu.ifsc.neabiAndroid.util.Resource
 
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
 fun HomeView(
     viewModel: HomeViewModel,
     navController: NavController,
 ) {
-    val scaffoldState = rememberScaffoldState()
-    val scope = rememberCoroutineScope()
     var searchText by remember { mutableStateOf("") }
-    var allCampus = viewModel.campus.collectAsState(initial = Resource.Loading(true))
-    Column(
-        modifier = Modifier.verticalScroll(rememberScrollState())
-    ) {
-        TextField(
-            value = searchText,
-            onValueChange = { searchText = it },
-            maxLines = 1,
-            singleLine = true,
-            modifier = Modifier.fillMaxWidth(),
-            placeholder = { Text("Buscar") },
-            colors = TextFieldDefaults.textFieldColors(
-                backgroundColor = Color(0xFFFFFFFF)
-            )
-        )
-        Spacer(modifier = Modifier.size(16.dp))
 
-        when(allCampus.value){
-            is Resource.Error -> {}
-            is Resource.Loading -> CircularProgressIndicator()
-            is Resource.Success -> {
-                allCampus.value.data?.forEach{
-                    CampusCard(navController = navController, campus = it)
-                }
-            }
+    val campusList = mutableStateListOf<Campus>()
+    campusList.addAll(viewModel.getCampus(null))
+
+    LazyColumn() {
+        item {
+            TextField(
+                value = searchText,
+                onValueChange = {
+                    searchText = it
+                    campusList.clear()
+                    campusList.addAll(viewModel.getCampus(it))
+                },
+                maxLines = 1,
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth(),
+                placeholder = { Text("Buscar") },
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color(0xFFFFFFFF)
+                )
+            )
+            Spacer(modifier = Modifier.size(16.dp))
         }
+        items(campusList) {
+            CampusCard(navController = navController, campus = it)
+        }
+
     }
 }
--- app/src/main/java/br/edu/ifsc/neabiAndroid/ui/home/HomeViewModel.kt
+++ app/src/main/java/br/edu/ifsc/neabiAndroid/ui/home/HomeViewModel.kt
@@ -4,15 +4,37 @@ import androidx.lifecycle.*
 import br.edu.ifsc.neabiAndroid.domain.model.Campus
 import br.edu.ifsc.neabiAndroid.domain.repository.HomeRepository
 import br.edu.ifsc.neabiAndroid.util.Resource
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.*
 import java.lang.IllegalArgumentException
+import kotlinx.coroutines.launch
 
 
 class HomeViewModel(private val rep: HomeRepository): ViewModel() {
 
     private val _campus: Flow<Resource<List<Campus>>> = rep.getCampus()
     val campus = _campus
+
+    var campusList = emptyList<Campus>()
+
+    init {
+        viewModelScope.launch {
+            campus.collect { resource ->
+                when (resource) {
+                    is Resource.Success -> {
+                        campusList = resource.data!!
+                    }
+                }
+
+            }
+        }
+    }
+
+    fun getCampus(searchOption: String?): List<Campus> {
+        if (searchOption != null) {
+            return campusList.filter { it.name.contains(searchOption, ignoreCase = true) }
+        }
+        return campusList
+    }
 }
 
 class HomeVMFactory(private val repository: HomeRepository) : ViewModelProvider.Factory{
