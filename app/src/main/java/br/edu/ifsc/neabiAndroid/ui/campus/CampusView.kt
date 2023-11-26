a15c606497956a165cb181b83c758802fd11c48a
Emanuel3k <emanuelsantos_677@hotmail.com>
2023-11-26 15:21:38 -0300

    Implementação do carrossel

--- app/src/main/java/br/edu/ifsc/neabiAndroid/ui/campus/CampusView.kt
+++ app/src/main/java/br/edu/ifsc/neabiAndroid/ui/campus/CampusView.kt
@@ -16,19 +16,19 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import br.edu.ifsc.neabiAndroid.data.remote.BaseURL.BASE_URL
 import androidx.navigation.NavController
 import br.edu.ifsc.neabiAndroid.R
+import br.edu.ifsc.neabiAndroid.data.remote.BaseURL.BASE_URL
 import br.edu.ifsc.neabiAndroid.ui.components.AddressInfo
 import br.edu.ifsc.neabiAndroid.ui.components.BoxInfo
 import br.edu.ifsc.neabiAndroid.ui.components.CardItem
+import br.edu.ifsc.neabiAndroid.ui.components.Carousel
 import br.edu.ifsc.neabiAndroid.ui.components.ExpandableCard
 import br.edu.ifsc.neabiAndroid.ui.theme.PrimaryColor
 import br.edu.ifsc.neabiAndroid.util.EmptyClass
 import br.edu.ifsc.neabiAndroid.util.sizeExtraLarge
 import br.edu.ifsc.neabiAndroid.util.sizeLarge
 import br.edu.ifsc.neabiAndroid.util.sizeMedium
-import coil.compose.SubcomposeAsyncImage
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberPermissionState
 import com.google.android.gms.maps.model.CameraPosition
@@ -43,28 +43,30 @@ fun CampusView(
     campusViewModel: CampusViewModel
 ) {
     val campus = campusViewModel.campus.observeAsState(EmptyClass.emptyCampus)
-    val permissionState = rememberPermissionState(permission = Manifest.permission.ACCESS_FINE_LOCATION)
+    val permissionState =
+        rememberPermissionState(permission = Manifest.permission.ACCESS_FINE_LOCATION)
 
     Column(
         modifier = Modifier.verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(sizeExtraLarge)
     ) {
-        if(campus.value.image.isBlank())
-            Image(painter = painterResource(id = R.drawable.default_campus_image),
-                contentDescription = "Imagem genérica do Campus")
-        else
-            SubcomposeAsyncImage(
-                model = BASE_URL+ campus.value.image,
-                loading = {
-                    CircularProgressIndicator()
-                },
-                contentDescription = "image",
+        if (campus.value.image.isEmpty()) {
+            Image(
+                painter = painterResource(id = R.drawable.default_campus_image),
+                contentDescription = "Imagem genérica do Campus"
             )
+        } else {
 
+            val urlList = campus.value.image.map { i ->
+                BASE_URL + i.photo
+            }
+
+            Carousel(images = urlList)
+        }
         Row(
             modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement =  Arrangement.SpaceEvenly
+            horizontalArrangement = Arrangement.SpaceEvenly
         ) {
             BoxInfo("Cursos", campus.value.courses.size)
             BoxInfo("Programas", campus.value.program.size)
@@ -76,47 +78,47 @@ fun CampusView(
             AddressInfo(campus.value.address)
         }
 
-        if(campus.value.courses.isNotEmpty())
+        if (campus.value.courses.isNotEmpty())
             ExpandableCard("Cursos") {
                 Column(verticalArrangement = Arrangement.spacedBy(sizeMedium)) {
                     for (course in campus.value.courses)
-                        CardItem(name = course.course.name){
+                        CardItem(name = course.course.name) {
                             navController.navigate("course/${course.pk}")
                         }
                 }
             }
 
-        if(campus.value.program.isNotEmpty())
+        if (campus.value.program.isNotEmpty())
             ExpandableCard("Programas") {
                 Column(verticalArrangement = Arrangement.spacedBy(sizeMedium)) {
-                    for(program in campus.value.program)
-                        CardItem(name = program.name){
-                            navController.navigate("home")
+                    for (program in campus.value.program)
+                        CardItem(name = program.name) {
+//                            navController.navigate("home")
                         }
                 }
             }
 
-        if(campus.value.project.isNotEmpty())
+        if (campus.value.project.isNotEmpty())
             ExpandableCard("Projetos") {
                 Column(verticalArrangement = Arrangement.spacedBy(sizeMedium)) {
-                    for(proj in campus.value.project)
-                        CardItem(name = proj.name){
-                            navController.navigate("home")
+                    for (proj in campus.value.project)
+                        CardItem(name = proj.name) {
+//                            navController.navigate("home")
                         }
                 }
             }
 
-        if(campus.value.studentAssistence.isNotEmpty())
+        if (campus.value.studentAssistence.isNotEmpty())
             ExpandableCard("Assistência Estudantil") {
                 Column(verticalArrangement = Arrangement.spacedBy(sizeMedium)) {
-                    for(action in campus.value.studentAssistence)
-                        CardItem(name = action.name){
-                            navController.navigate("home")
+                    for (action in campus.value.studentAssistence)
+                        CardItem(name = action.name) {
+//                            navController.navigate("home")
                         }
                 }
             }
 
-        Row(
+        /*Row(
             modifier = Modifier
                 .padding(start = sizeExtraLarge, end = sizeExtraLarge)
         ) {
@@ -128,14 +130,24 @@ fun CampusView(
                     .fillMaxWidth(),
                 contentAlignment = Alignment.Center
             ) {
-                if(permissionState.hasPermission){
-                    val mapCenter by campusViewModel.loc.collectAsState(initial = LatLng(-26.1833444,-50.3670326))
+                if (permissionState.hasPermission) {
+                    val mapCenter by campusViewModel.loc.collectAsState(
+                        initial = LatLng(
+                            -26.1833444,
+                            -50.3670326
+                        )
+                    )
                     val cameraPositionState = rememberCameraPositionState {
                         position = CameraPosition.fromLatLngZoom(mapCenter, 6.0f)
                     }
 
-                    LaunchedEffect(key1 = mapCenter){
-                        cameraPositionState.position = CameraPosition.fromLatLngZoom(LatLng(mapCenter.latitude,mapCenter.longitude),6.0f)
+                    LaunchedEffect(key1 = mapCenter) {
+                        cameraPositionState.position = CameraPosition.fromLatLngZoom(
+                            LatLng(
+                                mapCenter.latitude,
+                                mapCenter.longitude
+                            ), 6.0f
+                        )
                     }
 
                     GoogleMap(
@@ -144,7 +156,7 @@ fun CampusView(
                         properties = MapProperties(
                             isMyLocationEnabled = true
                         )
-                    ){
+                    ) {
                         Marker(
                             position = LatLng(
                                 campus.value.address.latitude.toDouble(),
@@ -152,10 +164,9 @@ fun CampusView(
                             )
                         )
                     }
-                }
-                else Text(text = "Sem permissão de GPS")
+                } else Text(text = "Sem permissão de GPS")
             }
-        }
+        }*/
 
         val uriHandler = LocalUriHandler.current
         Button(
